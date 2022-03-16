623d9bb8e4282cb7544e197848930380de3c5ba2
loto4uck <101703715+loto4uck@users.noreply.github.com>
2022-03-16 11:01:48 +0200

    Add files via upload

---
+++ zavd2.java
@@ -0,0 +1,15 @@
+package in_out;
+
+import java.util.Scanner;
+
+public class zavd2 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+        System.out.println("The next number for the number " + a + " is " + (a + 1) + ".");
+        System.out.println("The previous number for the number " + a + " is " + (a - 1) + ".");
+        
+
+    }
+
+}
