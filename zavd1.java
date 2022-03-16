20f51d6df467d447a938ff6ae282bfc58af2b275
loto4uck <101703715+loto4uck@users.noreply.github.com>
2022-03-16 10:58:16 +0200

    Add files via upload

---
+++ zavd1.java
@@ -0,0 +1,14 @@
+package in_out;
+
+import java.util.Scanner;
+
+public class zavd1 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	System.out.println((a * a) + (b * b));
+	}
+}
+
+
